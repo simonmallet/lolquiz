ed5e47812d7fe62c745acb51abec95333fb34408
Simon Mallet <simonmallet@simon-mallets-mac-pro.local>
2015-12-23 01:58:37 -0500

    basic chat system test

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('user connected', socket.id);
+    io.emit('system chat message', 'user ' + socket.id + ' connected!');
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+
+    socket.on('disconnect', function(){
+        io.emit('system chat message', 'user ' + socket.id + ' disconnected!');
+
+        console.log('user disconnected', socket.id);
+    });
 });
 
 http.listen(3000, function(){
